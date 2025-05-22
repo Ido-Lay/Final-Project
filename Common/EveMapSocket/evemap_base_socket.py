a55cebff2c725a2d3aab141b9d4422c538dac2e5
Owner <ido.eliavgames@gmail.com>
2025-05-22 21:08:51 +0300

    changed encryption

--- Common/EveMapSocket/evemap_base_socket.py
+++ Common/EveMapSocket/evemap_base_socket.py
@@ -22,27 +22,15 @@ class EveMapBaseSocket:
 
     def send_command(self, data: bytes, message_type: MessageType, packet_type: PacketType):
         packet = Packet.build_packet(data, message_type, packet_type)
-        encrypted_packet = self.encryptor.encrypt(packet)
-
-        length_bytes = len(encrypted_packet).to_bytes(4, byteorder='big')
-        self.tcp_socket.send(length_bytes + encrypted_packet)
+        packet.update_message(self.encryptor.encrypt(packet.message))
+        self.tcp_socket.send(packet.to_bytes())
 
     def recv_command(self) -> tuple[bytes, MessageType, PacketType]:
-        length_bytes = self.tcp_socket.recv(4)
-        if not length_bytes:
-            raise ConnectionError("Socket closed or no data received.")
-
-        encrypted_length = int.from_bytes(length_bytes, byteorder="big")
-
-        encrypted_data = b''
-        while len(encrypted_data) < encrypted_length:
-            chunk = self.tcp_socket.recv(encrypted_length - len(encrypted_data))
-            if not chunk:
-                raise ConnectionError("Incomplete encrypted data received.")
-            encrypted_data += chunk
-
-        decrypted_data = self.encryptor.decrypt(encrypted_data)
-        packet = Packet.from_bytes(decrypted_data)
-        return packet.message, packet.message_type, packet.packet_type
+        data_length = int.from_bytes(self.tcp_socket.recv(Packet.get_data_length_field_size()))
+        packet_type = PacketType(int.from_bytes(self.tcp_socket.recv(Packet.get_packet_type_field_size())))
+        message_type = MessageType(int.from_bytes(self.tcp_socket.recv(Packet.get_message_type_field_size())))
+        encrypted_message = self.tcp_socket.recv(data_length)
+        message = self.encryptor.decrypt(encrypted_message)
+        return message, message_type, packet_type
 
 
