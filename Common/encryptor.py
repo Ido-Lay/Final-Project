6fafbb03232b0b3b2dd48231da3b1096f3a2205e
Owner <ido.eliavgames@gmail.com>
2025-05-22 21:07:34 +0300

    fixed empty string

--- Common/encryptor.py
+++ Common/encryptor.py
@@ -1,15 +1,17 @@
 from cryptography.fernet import Fernet
-from Common.packet_base.eve_packet import Packet
 
 
 class Encryptor:
     def __init__(self, key: bytes):
         self.fernet = Fernet(key)
 
-    def encrypt(self, packet: Packet) -> bytes:
-        raw_data = packet.to_bytes()
-        return self.fernet.encrypt(raw_data)
+    def encrypt(self, data: bytes) -> bytes:
+        if data:
+            return self.fernet.encrypt(data)
+        return b''
 
     def decrypt(self, encrypted_data: bytes) -> bytes:
-        return self.fernet.decrypt(encrypted_data)
+        if encrypted_data:
+            return self.fernet.decrypt(encrypted_data)
+        return b''
 
